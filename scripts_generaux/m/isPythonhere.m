45db6ea38c5c934a350b28bcdab7faa8ea55da35
Robert Benoît <robert.benoit.47@gmail.com>
2016-12-09 09:41:00 -0500

    Improved isPythonhere

--- scripts_generaux/m/isPythonhere.m
+++ scripts_generaux/m/isPythonhere.m
@@ -2,16 +2,35 @@ function [ ok ] = isPythonhere( )
 %UNTITLED ispythonthere() checks for availability of Python, needed for
 %M_WEST
 %   Detailed explanation goes here
+persistent was_shown
+p=getenv('Python_Path');
 ok=false;
 cmd='python -V';
+if isempty(was_shown);was_shown=false;end
 %
 [status,cmdout]=system(cmd);
 if isequal (status, 0) && numel(cmdout) > 0
-    display (['python is available ', cmdout])
-    ok=true;
+    if ~was_shown;
+        display (['python is available ', cmdout])
+        was_shown=true;
+    end
+elseif exist(p,'dir')
+    display(['Checking for python.exe on your ''Python_Path'' ',p])
+    [status,z]=system(['cd C:\&C:&dir ',p,'\python.exe /s /b']);
+    if numel(z) > 0 && status == 0
+        display('Python executable was found in your ''Python_Path'' folder')
+        fprintf('%s\n',z)
+        display('Python is here')
+        was_shown=true;
+    else
+        display('Python executable was NOT found in your ''Python_Path'' folder')
+        APEmsg1('You MUST ADJUST your ''Python_Path'' folder','exit')
+    end
 else
-    APEmsg1('python is unavailable on your PC.  Required for M_WEST. Please install it','exit')
+    display('python is not on your current system PATH nor is your ''Python_Path'' a valid folder')
+    APEmsg1('python is unavailable on your PC.  Required for M_WEST. Please install it or adjust your Python_Path','exit')
     ok=false;
 end
+ok=true;
 end
 
